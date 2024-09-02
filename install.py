cb91dd1319958585841e9102b21bd107ecee69ad
kirill <150036551+programmist-kirill@users.noreply.github.com>
2024-09-02 16:50:08 +0600

    Create install.py

---
+++ install.py
@@ -0,0 +1,3 @@
+print('\t\taf9d-s090s-f9sd-0f9sd-0f9sd-fsd9f-0sdf9sd-0f9sd-f9s-v0s9-0v9sd-f0sd9f-0s9-ewf0-wgv8-saf-0asd8-as0df8asd0-fa8sdf-0asd89f-0asdf8a0sd-f8asd-089sdfs0')
+print('\t\tsopdfsd0-f8s-0vuis-0g98sdafsa[pfi[sadpvkasv[psiof[pasdfiaps[dfiaspf[iasfewfw=-f9d-=fas9f-=asf9sad-=f9sadf=-sa9fa=-vosa-=vf9asdf-09das=-fas9df=sd-')
+print('\t\ts-d0f89sd-0f-v0a9-=fasdf=-0samdvoipsduvfg[aspdfuposaidf[-adsif-asdifa-sdfisad9f-=9e2-=fisd[pfisadvpksap[ifsaer09iw-r09-0fas9df0-sdfka-wiififififi')
